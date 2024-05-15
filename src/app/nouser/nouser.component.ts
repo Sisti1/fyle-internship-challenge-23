24ff49e482cb2036202f9614d464596415683b8b
Sisti1 <shristigoyal516@gmail.com>
2024-05-15 14:52:42 +0530

    task done

---
+++ src/app/nouser/nouser.component.ts
@@ -0,0 +1,11 @@
+
+import { Component,Input } from '@angular/core';
+
+@Component({
+  selector: 'app-nouser',
+  templateUrl: './nouser.component.html',
+  styleUrls: ['./nouser.component.scss']
+})
+export class NouserComponent {
+  @Input() isSuccess: boolean = true;
+}
